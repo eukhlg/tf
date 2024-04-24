4e5493de8c17899e4c6a9d2a4a5888d88311acd5
Evgeny Khlgatyan <eukhlg@gmail.com>
2024-04-24 20:53:52 +0300

    TF for creating sa and key

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+    required_providers {
+        yandex = {
+        source  = "yandex-cloud/yandex"
+        version = "= 0.116.0"
+    }
+  }
+    required_version = "= 1.8.1"
+}
+
+provider "yandex" {
+    token                    = var.token
+    cloud_id                 = var.cloud_id
+    folder_id                = var.folder_id
+    zone                     = var.zone
+}
+
+resource "yandex_iam_service_account" "sa" {
+    folder_id = var.folder_id
+    name = "sa-sf"
+  
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+    folder_id = var.folder_id
+    role = "storage.editor"
+    member = "serviceAccount:${yandex_iam_service_account.sa.id}"
+  
+}
+
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+    service_account_id = yandex_iam_service_account.sa.id
+    description = "this is static access key for object storage"
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "token" {
+    description = "YC OAuth token"
+    type        = string   
+    default     = "token" # where to take? - https://yandex.cloud/en-ru/docs/iam/concepts/authorization/oauth-token?
+}
+
+# export TF_VAR_token=
+
+variable "cloud_id" {
+    description = "YC Cloud ID"
+    type        = string
+    default     = "cloud_id" # where to take? - Web UI or CLI  
+}
+
+# export TF_VAR_cloud_id=
+
+variable "folder_id" {
+    description = "YC Folder ID"
+    type        = string
+    default     = "folder_id" # where to take? - Web UI or CLI
+}
+
+# export TF_VAR_folder_id=
+
+variable "zone" {
+    description = "YC Compute Zone"
+    type        = string
+    default     = "ru-central1-b" # where to take? - Web UI or CLI
+}
