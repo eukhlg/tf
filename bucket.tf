7124bbdbf6a8ff47b84c029e8f2771133fc77262
Evgeny Khlgatyan <eukhlg@gmail.com>
2024-04-25 22:46:54 +0300

    removed old configs for lamp, lemp and bucket

--- bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "yandex_storage_bucket" "tf-bucket" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket = "tf-bucket-eukhlg"
-}
