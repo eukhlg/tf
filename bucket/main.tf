4a1e95e6203a02d643a743fd6f08fc093d02af65
Evgeny Khlgatyan <eukhlg@gmail.com>
2024-04-26 15:23:05 +0300

    added module for nlb

--- bucket/main.tf
+++ bucket/main.tf
@@ -10,4 +10,5 @@ resource "yandex_storage_bucket" "bucket" {
   access_key = var.access_key
   secret_key = var.secret_key
   bucket = var.bucket_name
+
 }
---
+++ nlb/main.tf
@@ -0,0 +1,48 @@
+terraform {
+    required_providers {
+        yandex = {
+        source  = "yandex-cloud/yandex"
+        }
+    }
+}
+
+resource "yandex_lb_target_group" "web-servers" {
+  name = var.tg_name
+
+    dynamic "target" {
+        for_each = var.targets
+        content {
+            subnet_id = target.value["subnet_id"]
+            address   = target.value["address"]
+    }
+    }
+
+}
+
+resource "yandex_lb_network_load_balancer" "one" {
+  name = var.nlb_name
+
+  listener {
+    name = var.listener_name
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+ attached_target_group {
+    target_group_id = yandex_lb_target_group.web-servers.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/"
+      }
+    }
+  }
+
+
+
+}
+
